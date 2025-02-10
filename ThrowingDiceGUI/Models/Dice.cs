776921b0e0053e9a86eba2d5b0d3f0cf5eaa0b6b
a21jeaha <hisui86@gmail.com>
2025-02-10 11:03:14 +0100

    added premade classes that need minimal changing

---
+++ ThrowingDiceGUI/Models/Dice.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ThrowingDiceGUI.ViewModels
+{
+
+	/**
+	 * The class represents a six sided die, and the rolling of said die. 
+	 **/
+
+	public class Dice
+	{
+		public int DiceValue { get; set; }
+
+		public Dice() 
+		{
+			DiceValue = 1; 
+		}
+
+		public int ThrowDice()
+		{
+			Random randomSeed = new Random();
+			
+			return DiceValue = randomSeed.Next(1, 7);
+		}
+	}
+}
---
+++ ThrowingDiceGUI/Models/Player.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ThrowingDiceGUI.ViewModels
+{
+	/** 
+	 * Player class: keeps track of current balance and bet.
+	 **/
+	class Player
+	{
+		private int deposit;
+		private int bet; 
+
+		public Player()
+		{
+			deposit = 0;
+			bet = 0;
+		}
+
+		public void SetBet(int bet)
+		{
+			this.bet = bet;
+		}
+
+		public int GetBet() 
+		{
+			return this.bet;
+		}
+
+		public void SetDeposit(int deposit) 
+		{
+			this.deposit = deposit;
+		}
+
+		public int GetDeposit() 
+		{
+			return this.deposit;
+		}
+	}
+}
