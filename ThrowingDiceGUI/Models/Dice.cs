17e6a6030dc11e2f3d1542e0d4af55e039a795f2
a21jeaha <hisui86@gmail.com>
2025-02-17 09:43:50 +0100

    Dice class updated to now also contain images related to the dice values
    it should dynamically update the image when the value is updated.
    not tested yet however.

--- ThrowingDiceGUI/Models/Dice.cs
+++ ThrowingDiceGUI/Models/Dice.cs
@@ -14,22 +14,48 @@ namespace ThrowingDiceGUI.Models
 	public class Dice
 	{
 		private int _DiceValue;
+		private string _DiceImagePath;
+		
 		public int DiceValue 
 		{
 			get => _DiceValue;
-			set => _DiceValue = value;
+			set 
+			{
+				_DiceValue = value;
+				UpdateDiceImage();
+			}
 		}
-		
+
+		// Declares DiceImagePath as a read-only property (only a getter, no set method).				
+		public string DiceImagePath => _DiceImagePath;  
+
 		public Dice() 
 		{
-			DiceValue = 1; 
+			DiceValue = 1;
+		}
+
+
+		private void UpdateDiceImage()
+		{
+			_DiceImagePath = DiceValue switch
+			{
+				1 => "avares://ThrowingDiceGUI/Assets/dice (1).png",
+				2 => "avares://ThrowingDiceGUI/Assets/dice (2).png",
+				3 => "avares://ThrowingDiceGUI/Assets/dice (3).png",
+				4 => "avares://ThrowingDiceGUI/Assets/dice (4).png",
+				5 => "avares://ThrowingDiceGUI/Assets/dice (5).png",
+				6 => "avares://ThrowingDiceGUI/Assets/dice (6).png",
+				_ => "avares://ThrowingDiceGUI/Assets/dice (Error).png",
+			};
 		}
 
 		public int ThrowDice()
 		{
 			Random randomSeed = new Random();
-			
-			return DiceValue = randomSeed.Next(1, 7);
+			DiceValue = randomSeed.Next(1, 7);
+
+
+			return DiceValue;
 		}
 	}
 }
